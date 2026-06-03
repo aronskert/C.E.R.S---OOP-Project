da4ca77773011ab2bb4a89690fadfdad380b7357
aronskert <aaronkertespedido@gmail.com>
2026-06-03 23:10:54 +0800

    Merge origin/master
    # Conflicts:
    #	src/ERS_NU/ContactInfo.form
    #	src/ERS_NU/ContactInfo.java

--- src/ERS_NU/Database.java
+++ src/ERS_NU/Database.java
@@ -169,9 +169,16 @@ public void reservation_data() {
             Class[] types = new Class [] {
                 java.lang.String.class, java.lang.Object.class, java.lang.Object.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.Object.class, java.lang.Object.class
             };
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false, false, false, false, false, false
+            };
 
             public Class getColumnClass(int columnIndex) {
                 return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
             }
         });
         jTable1.setToolTipText("");
